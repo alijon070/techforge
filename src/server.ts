a1c3159ee69229deac63d9b812dc1e8f97d082c5
Albert <muminovali648@gmail.com>
2026-08-06 22:28:49 +0900

    feat: integration of enviromental variables

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
+// import app from "./app";
+
+console.log("MONGO_URL is:", process.env.MONGO_URL);
+mongoose
+  .connect(String(process.env.MONGO_URL), {})
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
