f9d9d492ea19f476e4b40bb707c740eb537cdf1e
Albert <muminovali648@gmail.com>
2026-08-18 00:26:36 +0900

    feat: develop getMyOrders rest api

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,6 +3,8 @@ import { T } from "../libs/types/common";
 import Errors, { HttpCode } from "../libs/types/Errors";
 import { ExtentedRequest } from "../libs/types/member";
 import OrderService from "../models/Order.Service";
+import { OrderInquiry } from "../libs/types/order";
+import { OrderStatus } from "../libs/enums/order.enum";
 
 const orderService = new OrderService();
 const orderController: T = {};
@@ -15,6 +17,27 @@ orderController.createOrder = async (req: ExtentedRequest, res: Response) => {
     res.status(HttpCode.CREATED).json(result);
   } catch (err) {
     console.log("Error, createOrder", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+orderController.getMyOrders = async (req: ExtentedRequest, res: Response) => {
+  try {
+    console.log("getMyOrders");
+    const { page, limit, orderStatus } = req.query;
+
+    const inquiry: OrderInquiry = {
+      page: Number(page),
+      limit: Number(limit),
+      orderStatus: orderStatus as OrderStatus,
+    };
+
+    const result = await orderService.getMyOrders(req.member, inquiry);
+
+    res.status(HttpCode.CREATED).json(result);
+  } catch (err) {
+    console.log("Error, getMyOrders", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
   }
--- src/models/Order.Service.ts
+++ src/models/Order.Service.ts
@@ -2,7 +2,7 @@ import { ObjectId } from "mongoose";
 import { shapeIntoMongooseObjectId } from "../libs/types/config";
 import Errors, { HttpCode, Message } from "../libs/types/Errors";
 import { Member } from "../libs/types/member";
-import { Order, OrderItemInput } from "../libs/types/order";
+import { Order, OrderInquiry, OrderItemInput } from "../libs/types/order";
 import OrderModel from "../schema/Order.model";
 import OrderItemModel from "../schema/OrderItem.model";
 
@@ -52,6 +52,43 @@ class OrderService {
     });
 
     const orderItemsState = await Promise.all(promisedList);
+  }
+
+  public async getMyOrders(
+    member: Member,
+    inquiry: OrderInquiry
+  ): Promise<Order[]> {
+    const memberId = shapeIntoMongooseObjectId(member._id);
+    const matches = { memberId: memberId, orderStatus: inquiry.orderStatus };
+
+    const result = await this.orderModel
+      .aggregate([
+        { $match: matches },
+        { $sort: { updatedAt: -1 } },
+        { $skip: (inquiry.page - 1) * inquiry.limit },
+        { $limit: inquiry.limit },
+        {
+          $lookup: {
+            from: "orderItems",
+            localField: "_id",
+            foreignField: "orderId",
+            as: "orderItems",
+          },
+        },
+        {
+          $lookup: {
+            from: "products",
+            localField: "orderItems.productId",
+            foreignField: "_id",
+            as: "productData",
+          },
+        },
+      ])
+      .exec();
+
+    if (!result.length)
+      throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    return result;
   }
 }
 
